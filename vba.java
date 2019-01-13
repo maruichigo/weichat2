b0eb9d0e5ff1d1b6df0a30b45f39c4cc8b9af773
maruichigo <maleilm@cn.ibm.com>
2019-01-14 00:22:26 +0800

    Update vba.java

--- vba.java
+++ vba.java
@@ -21,3 +21,31 @@ https://blog.csdn.net/babylove_BaLe/article/details/73201115
     public void setPageSessionBean(PageSessionBean pageSessionBean) {
         this.pageSessionBean = pageSessionBean;
     }
+
+
+
+
+
+    public void init() {
+        // 戻ってきた場合
+        if (pageSessionBean.getPageConData("Mst501") != null) {
+            pageBean.setValues(pageSessionBean.getPageConData("Mst501"));
+            pageBean.setTestText((String) pageSessionBean.getPageConData("Mst501").get("detail").get("SHIMUKE_CHI_MEI"));
+        }
+        // 進んできた場合
+        System.out.println("init!");
+    }
+    
+    /**
+     * 後処理
+     * @param bean
+     * @param event
+     * @param messageData
+     * @param backFlag
+     */
+//    public void search(SSNaviManagedBean bean, FacesEvent event, MessageDataModel messageData, Boolean backFlag) {
+    public void search() {
+    String str;
+    pageSessionBean.setPageConData("Mst501",pageBean.getValues());
+        str = "test";    
+    }
