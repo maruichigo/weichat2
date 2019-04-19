3a812eee7b0ec4d694468c8ca224e7a7b929629d
maruichigo <maleilm@cn.ibm.com>
2019-04-19 18:41:54 +0900

    Update vba.java

--- vba.java
+++ vba.java
@@ -1,4 +1,6 @@
 https://blog.csdn.net/babylove_BaLe/article/details/73201115
 https://www.cnblogs.com/sebastian-tyd/p/7895182.html
+
+https://www.updatestar.com/directdownload/tera-term/2037688
 https://pan.baidu.com/s/1RWKvUxC9e_pP67speLtIcA
 
