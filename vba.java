ffde67b4eab3d4b50b8be0a2c6babadf362e6ede
maruichigo <maleilm@cn.ibm.com>
2019-06-27 19:31:20 +0900

    Update vba.java

--- vba.java
+++ vba.java
@@ -14,3 +14,12 @@ https://www.oschina.net/p/ruoyi
 http://www.intra-mart.jp/e-learning/training/02_java_free_tera/index.html
 
 http://www.tuniu.com/tucom/210462170
+
+
+exp system/123456@172.16.192.42:1523/sdc_test02 file='C:/sdc_test02.dmp'
+
+alter user system identified by 123456
+create user sdc_test02 identified by sdc_test02;
+grant connect, resource to sdc_test02;
+
+imp system/123456@localhost:1521/sdcdb fromuser=system touser=system file='C:/sdc_test02.dmp' ignore=y
